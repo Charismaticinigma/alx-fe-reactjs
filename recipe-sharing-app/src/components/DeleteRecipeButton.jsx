aa8a6505ff505078c4d7c005436764031531dbf1
Boniface Mwangi <wahomemwangi222@gmail.com>
2024-09-07 17:19:43 +0300

    added files and folders

---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,19 @@
+
+import { useNavigate } from 'react-router-dom';
+import useRecipeStore from "./recipeStore";
+
+const DeleteButton = ({ id }) => {
+  const navigate = useNavigate();
+  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+
+  function handleDelete() {
+    deleteRecipe(id);
+    navigate('/');
+  }
+
+  return (
+    <button className="delete" onClick={handleDelete}>Delete</button>
+  );
+}
+
+export default DeleteButton;
